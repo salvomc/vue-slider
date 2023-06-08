5c0d917600d291b346faf64440145a8fcd8c501a
salvomc <smcrwrk@gmail.com>
2023-06-08 16:45:15 +0200

    slider done

--- js/script.js
+++ js/script.js
@@ -1,25 +1,82 @@
-const slides = [
-{
-    image: 'img/01.webp',
-        title: 'Marvel\'s Spiderman Miles Morale',
-        text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
-    }, {
-        image: 'img/02.webp',
-        title: 'Ratchet & Clank: Rift Apart',
-        text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
-    }, {
-        image: 'img/03.webp',
-        title: 'Fortnite',
-        text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
-    }, {
-        image: 'img/04.webp',
-        title: 'Stray',
-        text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
-    }, {
-        image: 'img/05.webp',
-        title: "Marvel's Avengers",
-        text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
-    }
-];
+const {createApp} = Vue;
 
-console.log(slides);
+createApp({
+        data(){
+            return{
+                    slides : [
+                                {
+                                    image: 'img/01.webp',
+                                    title: 'Marvel\'s Spiderman Miles Morale',
+                                    text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+                                }, 
+                                {
+                                    image: 'img/02.webp',
+                                    title: 'Ratchet & Clank: Rift Apart',
+                                    text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+                                }, 
+                                {
+                                    image: 'img/03.webp',
+                                    title: 'Fortnite',
+                                    text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+                                }, 
+                                {
+                                    image: 'img/04.webp',
+                                    title: 'Stray',
+                                    text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+                                }, 
+                                {
+                                    image: 'img/05.webp',
+                                    title: "Marvel's Avengers",
+                                    text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+                                }
+                             ],     
+                             
+                             activeImage:0,      
+                             autoScroll: false,
+                    }
+                
+                },created(){
+                                this.startAutoscroll();
+                           },
+        methods: {
+                    changeImage(index)
+                    {
+                        this.activeImage = index;
+                        console.log(index)
+                        index++
+                    },
+                    
+                    nextImage()
+                    {
+                        this.activeImage++;
+                        if(this.activeImage > this.slides.length -1)
+                        {
+                            this.activeImage=0
+                        }
+                    },
+                    
+                    previousImage()
+                    {
+                        this.activeImage--;
+                        if(this.activeImage < 0)
+                        {
+                            this.activeImage= this.slides.length -1
+                        }
+                    },
+       
+                    startAutoscroll()
+                    {
+                        this.autoScroll= setInterval(()=> 
+                        {
+                            this.nextImage()
+                        }, 1500)
+                    },
+                    
+                    stopAutoscroll()
+                    {
+                        clearInterval(this.autoScroll)
+                    }
+
+                 },
+                        
+}).mount('#app');
